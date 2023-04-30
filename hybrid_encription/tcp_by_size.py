05d2c6af55ff66b384f0504fca7da9bc79d6ad7f
HilaShaked <121008561+HilaShaked@users.noreply.github.com>
2023-04-30 20:20:13 +0300

    was updated a bit

--- hybrid_encription/tcp_by_size.py
+++ hybrid_encription/tcp_by_size.py
@@ -5,7 +5,7 @@ size_header_size = len(SIZE_HEADER_FORMAT)
 TCP_DEBUG = True
 
 
-def recv_by_size(sock, *, addition = None, down_a_line = True, return_type="string"):
+def recv_by_size(sock, *, addition_before = None, addition_after = '', down_a_line = True, return_type="string"):
     """
     ** returns a tuple with the message received and the size (so I could send size related errors) **
     """
@@ -13,7 +13,11 @@ def recv_by_size(sock, *, addition = None, down_a_line = True, return_type="stri
     """Get len of message"""
     str_size, data_len = b"", 0
     while len(str_size) < size_header_size:
-        _d = sock.recv(size_header_size - len(str_size))
+        try:
+            _d = sock.recv(size_header_size - len(str_size))
+        except ConnectionResetError:
+            return b'', 0
+
         if len(_d) == 0:  # if not all the size of the message arrived
             str_size = b""
             break
@@ -45,9 +49,9 @@ def recv_by_size(sock, *, addition = None, down_a_line = True, return_type="stri
                 pass
         if down_a_line:
             print()
-        if addition is not None:
-            print(addition, end='')
-        print(f"Receive({str_size})>>>{data_to_print}")
+        if addition_before is not None:
+            print(addition_before, end='')
+        print(f"Receive({str_size}){addition_after}<<<{data_to_print}")
 
 
     if data_len != len(data):
@@ -57,7 +61,7 @@ def recv_by_size(sock, *, addition = None, down_a_line = True, return_type="stri
     return data, data_len
 
 
-def send_with_size(sock, data, down_a_line = True):
+def send_with_size(sock, data, *, down_a_line = True, addition_before = ''):
     len_data = str(len(data)).zfill(size_header_size)  # get len of data
     len_data = len_data.encode()
     if type(data) != bytes:  # encode data if necessary
@@ -75,129 +79,5 @@ def send_with_size(sock, data, down_a_line = True):
                 pass
         if down_a_line:
             print()
-        print(f"Sent({len_data})>>>{data}\n")
+        print(f"{addition_before}Sent({len_data})>>>{data[8:]}\n")
 
-
-def __hex(s):
-    print("in")
-    cnt = 0
-    for i in range(len(s)):
-        if cnt % 16 == 0:
-            print("")
-        elif cnt % 8 == 0:
-            print("    ", end='')
-        cnt += 1
-        print("%02X" % int(ord(s[i])), end='')
-
-
-"""
-#
-#
-#
-Binary Size by 4 bytes   from 1 to 4GB
-#
-#
-#
-"""
-
-
-def send_one_message(sock, data):
-    """
-    Send a message to the socket.
-    """
-    # sock.sendall(struct.pack('!I', len(message)) + message)
-    try:
-        length = socket.htonl(len(data))
-        if type(data) != bytes:
-            data = data.encode()
-        sock.sendall(struct.pack('I', length) + data)
-        data_part = data[:100]
-        if TCP_DEBUG and len(data) > 0:
-            print(f"\nSent({len(data)})>>>{data_part}")
-    except Exception as e:
-        print(f"ERROR in send_one_message\n{e}")
-
-
-def recv_one_message(sock, return_type="string"):
-    """
-    Receive one message by two steps 4 bytes and all rest.
-    """
-    len_section = __recv_amount(sock, 4)
-    if not len_section:
-        return None
-    len_int, = struct.unpack('I', len_section)
-    len_int = socket.ntohl(len_int)
-
-    data = __recv_amount(sock, len_int)
-    if TCP_DEBUG and len(data) != 0:
-        print(f"\nRecv({len_int})>>>{data[:100]}\n")
-
-    if len_int != len(data):
-        data = b''  # Partial data is like no data !
-    if return_type == "string":
-        return data.decode()
-
-    return data
-
-
-def __recv_amount(sock, size=4):
-    buffer = b''
-    while size:
-        new_bufffer = sock.recv(size)
-        if not new_bufffer:
-            return b''
-        buffer += new_bufffer
-        size -= len(new_bufffer)
-    # __hex(buffer)
-    return buffer
-
-
-"""
-Unit Test Section
-just for test 
-"""
-
-
-def main_for_test(role):
-    import socket
-    import time
-    port = 12312
-    if role == 'srv':
-        s = socket.socket()
-        s.bind(('0.0.0.0', port))
-        s.listen(1)
-        cli_s, addr = s.accept()
-        data = recv_by_size(cli_s)
-        print("1 server got:" + data)
-        send_with_size(cli_s, "1 back:" + data)
-        time.sleep(3)
-
-        print("\n\n\nServer Binary Sction\n")
-        data = recv_one_message(cli_s)
-        print("2 server got:" + data)
-        send_one_message(cli_s, "2 back:" + data)
-
-        cli_s.close()
-        time.sleep(3)
-        s.close()
-    elif role == 'cli':
-        c = socket.socket()
-        c.connect(('127.0.0.1', port))
-        send_with_size(c, "ABC")
-
-        print("1 client got:" + recv_by_size(c))
-        time.sleep(3)
-
-        print("\n\n\nClient Binary Sction\n")
-        send_one_message(c, "abcdefghijklmnop")
-
-        print("2 client got:" + recv_one_message(c))
-        time.sleep(3)
-        c.close()
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) >= 2:
-        main_for_test(sys.argv[1])
